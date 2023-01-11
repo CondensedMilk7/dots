917e26d81141b641b15237c9df4ed9bd7bfa6a7c
Pridon Tetradze <pridontetradze@gmail.com>
2023-01-11 22:09:54 +0400

    change clone script

--- .local/bin/clone_config.sh
+++ .local/bin/clone_config.sh
@@ -1,16 +1,14 @@
-#!/bin/sh
-
 git clone --bare https://github.com/CondensedMilk7/dots.git $HOME/.cfg
-function {
-  /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 mkdir -p .config-backup
 config checkout
 if [ $? = 0 ]; then
-  echo "Checked out config";
-else
-  echo "Backing up pre-existing dotfiles";
-  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 config checkout
-config status.showUntrackedFiles no
+config config status.showUntrackedFiles no
